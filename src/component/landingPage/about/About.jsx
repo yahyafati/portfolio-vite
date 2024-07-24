d73d56793765030090e6fbee25e04d968169d2ce
Yahya Fati <yfati037@gmail.com>
2024-07-24 13:57:05 +0300

    Modularized About.jsx

--- src/component/landingPage/about/About.jsx
+++ src/component/landingPage/about/About.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import hiFromDev from "../../../svg/About.svg";
-import "../../../style/about/about.scss";
+import * as style from "./style.module.scss"
 
 const About = ({_ref}) => {
     return (
-        <div ref={_ref} id="aboutSection">
-            <div className="imgContainer">
+        <div ref={_ref} id="aboutSection" className={style.aboutSection}>
+            <div className={style.imgContainer}>
                 <img
-                    className="aboutImg"
+                    className={style.aboutImg}
                     src={hiFromDev}
                     alt="A sitting developer waving hi."
                 />
             </div>
-            <div className="aboutText">
+            <div className={style.aboutText}>
                 <h2>
-                    What can I say! I just <span className="love">love</span> to
-                    <span className="code"> code</span>.
+                    What can I say! I just <span className={style.love}>love</span> to
+                    <span className={style.code}> code</span>.
                 </h2>
-                <p className="about">
+                <p className={style.about}>
                     Hi, I’m Yahya. I’m the{" "}
                     <strong>Passionate Programmer</strong>. I love builiding
                     stuff (Software stuff obviously). I’m a Fullstack Web
