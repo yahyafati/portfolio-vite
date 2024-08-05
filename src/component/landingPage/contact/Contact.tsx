b2b6dd64f635cb106035bef8e6ab8b3ddd7a4bdc
Yahya Fati <yfati037@gmail.com>
2024-08-05 09:30:56 +0300

    Localized shall we and footer

--- src/component/landingPage/contact/Contact.tsx
+++ src/component/landingPage/contact/Contact.tsx
@@ -1,13 +1,26 @@
 import SocialMediaIcons from './SocialMediaIcons';
 
 import style from './style.module.scss';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 const Contact = () => {
+    const intl = useIntl();
+    const fullName =
+        intl.formatMessage({ id: 'firstName' }) +
+        ' ' +
+        intl.formatMessage({ id: 'lastName' });
     return (
         <footer id="contactSection" className={style.contactSection}>
-            <h2 className={style.name}>Yahya Fati Haji</h2>
+            <h2 className={style.name}>{fullName}</h2>
             <SocialMediaIcons />
-            <h6 className={style.credit}>Designed and Developed By Me</h6>
+            <h6 className={style.credit}>
+                <FormattedMessage
+                    id="landing.footer.credit"
+                    values={{
+                        designer: intl.formatMessage({ id: 'firstName' }),
+                    }}
+                />
+            </h6>
         </footer>
     );
 };
--- src/component/landingPage/shallWe/ShallWe.tsx
+++ src/component/landingPage/shallWe/ShallWe.tsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom';
 
 import style from './style.module.scss';
+import { FormattedMessage } from 'react-intl';
 
 const ShallWe = () => {
     return (
         <div id="shallWeSection" className={style.shallWeSection}>
             <hr className={style.hLine} />
             <Link to={'/contact'} className={style.textContainer}>
-                <h4 className={style.shallWe}>Shall We Start?</h4>
-                <p className={style.letsGo}>Let's Go</p>
+                <h4 className={style.shallWe}>
+                    <FormattedMessage id="landing.shallWe.title" />
+                </h4>
+                <p className={style.letsGo}>
+                    <FormattedMessage id="landing.shallWe.button" />
+                </p>
             </Link>
             <hr className={style.hLine} />
         </div>
