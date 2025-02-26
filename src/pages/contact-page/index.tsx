64176c7b5a05168e740b533f9958148b74e5282d
Yahya Fati <yfati037@gmail.com>
2025-02-26 21:24:08 +0300

    Added logo on contact page

--- src/pages/contact-page/index.tsx
+++ src/pages/contact-page/index.tsx
@@ -2,12 +2,31 @@ import ContactForm from '@/component/contact/contact-form';
 import ContactSVG from '@svg/Contact.svg?react';
 
 import style from './style.module.scss';
+import { useIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
 
 const ContactPage = () => {
+    const { formatMessage: intlFmt } = useIntl();
+
+    const fullName =
+        intlFmt({
+            id: 'landing.home.firstName',
+            defaultMessage: 'Yahya',
+        }) +
+        ' ' +
+        intlFmt({
+            id: 'landing.home.lastName',
+            defaultMessage: 'Haji',
+        });
     return (
-        <div className={style.contactPage}>
-            <ContactSVG className={style.contactImg} />
-            <ContactForm />
+        <div id="contact" className={style.contactSection}>
+            <h2 className={style.title}>
+                <Link to={'/'}>{fullName}</Link>
+            </h2>
+            <div className={style.content}>
+                <ContactSVG className={style.contactImg} />
+                <ContactForm />
+            </div>
         </div>
     );
 };
