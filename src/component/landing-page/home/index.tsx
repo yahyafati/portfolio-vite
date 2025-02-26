8397f4483a03da05b70b24f8dbaa8b9de2cc5fa6
Yahya Fati <yfati037@gmail.com>
2025-02-26 19:47:32 +0300

    Fixed localization

--- src/component/landing-page/home/index.tsx
+++ src/component/landing-page/home/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import style from './style.module.scss';
@@ -7,61 +6,55 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import { FaAngleDoubleDown } from 'react-icons/fa';
 import { useScrollY } from '@/hooks';
 
-const MY_TITLES_LENGTH = 4;
 const Home = () => {
     const { formatMessage: intlFmt } = useIntl();
-    const [currentWordIndex, setCurrentWordIndex] = useState(0);
-    const [currentLetterIndex, setCurrentLetterIndex] = useState(1);
     const scrollY = useScrollY();
     const HIDE_SCROLL_DOWN_BTN_Y_THRESHOLD = 30;
 
-    const MY_TITLE_ALL = Array.from({ length: MY_TITLES_LENGTH }, (_, i) =>
-        intlFmt({ id: `landing.home.titles.title${i + 1}`, defaultMessage: '' })
-    );
-
-    const MY_TITLE = MY_TITLE_ALL.filter((title) => title !== '');
-
-    if (MY_TITLE.length !== MY_TITLES_LENGTH) {
-        throw new Error(
-            'The length of the title in the translation file is not equal to MY_TITLES_LENGTH'
-        );
-    }
-
-    useEffect(() => {
-        const interval = setTimeout(() => {
-            const length = MY_TITLE[currentWordIndex].length;
-            const buffer = 30;
-            if (currentLetterIndex < length + buffer) {
-                setCurrentLetterIndex((current) => current + 1);
-            } else {
-                setCurrentLetterIndex(1);
-                setCurrentWordIndex(
-                    (current) => (current + 1) % MY_TITLE.length
-                );
-            }
-        }, 125);
-
-        return () => clearTimeout(interval);
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentLetterIndex]);
-
+    const fullName =
+        intlFmt({
+            id: 'landing.home.firstName',
+            defaultMessage: 'Yahya',
+        }) +
+        ' ' +
+        intlFmt({
+            id: 'landing.home.lastName',
+            defaultMessage: 'Haji',
+        });
     return (
         <div id="home" className={style.homeSection}>
             <div className={style.mainContent}>
-                <p className={style.hi}>Hi, I'm </p>
-                <h1 className={style.name}>Yahya Haji</h1>
+                <p className={style.hi}>
+                    <FormattedMessage
+                        id="landing.home.hi"
+                        defaultMessage="Hi, I'm "
+                    />
+                </p>
+                <h1 className={style.name}>{fullName}</h1>
                 <h2 className={style.work}>
-                    Consultant, Developer, and Designer
+                    <FormattedMessage
+                        id="landing.home.work"
+                        defaultMessage="Consultant, Developer, and Designer"
+                    />
                 </h2>
-                <button className={classNames(style.cta, style.primary)}>
-                    <Link to="/contact">
-                        <FormattedMessage
-                            id="landing.home.contactMe"
-                            defaultMessage="Contact Me"
-                        />
-                    </Link>
-                </button>
+                <div className={style.ctaContainer}>
+                    <button className={classNames(style.cta, style.primary)}>
+                        <Link to="/contact">
+                            <FormattedMessage
+                                id="landing.home.contactMe"
+                                defaultMessage="Contact Me"
+                            />
+                        </Link>
+                    </button>
+                    <button className={style.cta}>
+                        <Link to="/Resume.pdf" target="_blank">
+                            <FormattedMessage
+                                id="landing.home.getResume"
+                                defaultMessage="Get Resume"
+                            />
+                        </Link>
+                    </button>
+                </div>
             </div>
 
             <button
@@ -75,7 +68,11 @@ const Home = () => {
                         ?.scrollIntoView({ behavior: 'smooth' });
                 }}
             >
-                Scroll Down <FaAngleDoubleDown />
+                <FormattedMessage
+                    id="landing.home.scrollDown"
+                    defaultMessage={'Scroll Down'}
+                />
+                <FaAngleDoubleDown />
             </button>
         </div>
     );
